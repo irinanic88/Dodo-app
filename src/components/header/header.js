076f9ed330eba2cd2bfe06c2f7518afe1b354d3a
Irina Nicolai <irina.nic.yu@gmail.com>
2021-04-24 16:58:23 +0200

    Added Button and CloseButton

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../button';
 import styles from './header.module.css';
+import CloseButton from '../button/closeButton';
 
 const Header = () => {
     return(
