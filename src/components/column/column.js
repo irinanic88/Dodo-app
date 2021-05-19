71e4abcea8bbd2da2c78dcf537200e20f2d0ea0a
Irina Nicolai <irina.nic.yu@gmail.com>
2021-05-19 16:58:06 +0200

    return droppable to whole column element

--- src/components/column/column.js
+++ src/components/column/column.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Droppable} from 'react-beautiful-dnd';
 import cn from 'classnames';
 import Ticket from '../ticket';
-import { ticketsForColumnSelector } from '../../redux/selectors';
+import {ticketsForColumnSelector} from '../../redux/selectors';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 import styles from './column.module.css';
 
-const Column = ({title, tickets}) => {
-    return(
-       
+const Column = ({tickets, title}) => {
+        return (
             <Droppable droppableId={title}>
                 {(provided, snapshot) => {
                     const isDraggingOver = snapshot.isDraggingOver;
                     return (
                         <div className={cn(styles.column, {[styles.overColumn]: isDraggingOver})}
-                                ref={provided.innerRef}  
-                                {...provided.droppableProps}
+                             ref={provided.innerRef}
+                             {...provided.droppableProps}
                         >
-                            <div className={cn(styles.header, {[styles.overColumn]: isDraggingOver})}>
+                            <div className={styles.header}>
                                 <h2>{title}</h2>
                             </div>
                             <div className={styles.headerBottomLine}></div>
                             <div className={styles.body}>
-                                {tickets.map((ticket, index) => <Ticket key={ticket.id} ticket={ticket} index={index}/>)}
+                                {tickets.map((ticket, index) => <Ticket key={ticket.id}
+                                                                        ticket={ticket}
+                                                                        index={index}/>)}
                                 {provided.placeholder}
                             </div>
-                    </div> 
-                    );
+                        </div>
+                        );
                     }
                 }
             </Droppable>
-    );
+        );
 };
 
 Column.propTypes = {
@@ -44,5 +45,4 @@ const mapStateToProps = (state, props) => ({
     tickets: ticketsForColumnSelector(state, props),
 });
 
-export default connect(mapStateToProps) (Column);
-
+export default connect(mapStateToProps)(Column);
