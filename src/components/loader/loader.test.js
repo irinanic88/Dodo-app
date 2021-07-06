3582ba6d16a02050f0adc49f63ec3046f7d66c60
Irina Nicolai <irina.nic.yu@gmail.com>
2021-07-06 17:50:04 +0200

    Added loader tests

---
+++ src/components/loader/loader.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Enzyme, {mount} from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import Loader from './loader';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Loader', () => {
+   it('should render', () => {
+      const wrapper = mount(<Loader />);
+      expect(wrapper.find('[data-id="loader"]').length).toBe(1);
+   });
+});
